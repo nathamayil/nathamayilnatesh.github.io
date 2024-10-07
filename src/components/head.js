827faa511e2c8fabe2cce0023b6ed72fadada229
Nathamayil Natesh <nathamayil@gmail.com>
2024-10-07 20:17:25 +0530

    Changes to head.js

--- src/components/head.js
+++ src/components/head.js
@@ -14,11 +14,8 @@ const Head = ({ title, description, image }) => {
       query {
         site {
           siteMetadata {
-            defaultTitle: title
-            defaultDescription: description
-            siteUrl
-            defaultImage: image
-            twitterUsername
+            defaultTitle: "Mayil"
+            defaultDescription: "Mayil's personal website"
           }
         }
       }
@@ -45,19 +42,10 @@ const Head = ({ title, description, image }) => {
       <html lang="en" />
 
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
 
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.image} />
-      <meta property="og:url" content={seo.url} />
       <meta property="og:type" content="website" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:title" content={seo.title} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
 
       <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
     </Helmet>
@@ -69,11 +57,9 @@ export default Head;
 Head.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  image: PropTypes.string,
 };
 
 Head.defaultProps = {
   title: null,
   description: null,
-  image: null,
 };
